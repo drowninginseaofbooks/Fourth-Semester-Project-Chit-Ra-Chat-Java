756931738a4f411390bfd1e28db1123e3c50a741
Somu👋 <153536896+somu-sapkota@users.noreply.github.com>
2024-07-29 22:05:26 +0545

    Add files via upload

--- Chit-Ra-Chat/Server/Chit-Ra-Chat/Server/src/Database.java
+++ Chit-Ra-Chat/Server/Chit-Ra-Chat/Server/src/Database.java
@@ -1,40 +1,8 @@
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.PreparedStatement;
-import java.sql.ResultSet;
-//import java.sql.ResultSet;
-import java.sql.SQLException;
-import java.sql.Statement;
 
-public class Database {
-   
-  
-       
+public class Database { 
     String URL = "jdbc:mysql://localhost:3306/chit-ra-chat";
     String db_user = "root";
     String db_password = "@s*Omu12";
-    private Statement statement;
-    Connection conn;
-            
+
     
-    public Database(){
-
-            try 
-            {
-                //loading the driver to the sql
-                Class.forName("com.mysql.cj.jdbc.Driver");
-            } 
-
-            catch (ClassNotFoundException e)
-            {
-                e.printStackTrace();
-            }
-        }
 }
-
-
-
-      
-
-
-
