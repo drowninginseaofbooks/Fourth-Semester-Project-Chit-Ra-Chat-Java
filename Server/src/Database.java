41492ff5ae44bd83d6de8e99f7b514129b4965b0
Somu👋 <153536896+somu-sapkota@users.noreply.github.com>
2024-07-30 12:23:43 +0545

    database connection is built with the driver

--- Server/src/Database.java
+++ Server/src/Database.java
@@ -1,10 +1,41 @@
-//import java.sql.Connection;
-//import java.sql.Statement;
 
-public class Database {
+import java.sql.Connection;
+import java.sql.DriverManager;
+//import java.sql.PreparedStatement;
+//import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+public class Database{
    
-        String URL = "jdbc:mysql://localhost:3306/chit-ra-chat";
-        String db_user = "root";
-        String db_password = "@s*Omu12";
-     
-}
+    String URL = "jdbc:mysql://localhost:3306/chit-ra-chat";
+    String db_user = "root";
+    String db_password = "@s*Omu12";
+    private Statement statement;
+    Connection conn;
+        
+
+    public Database(){
+        try {
+            try {
+                //loading the driver to the sql
+                Class.forName("com.mysql.cj.jdbc.Driver");
+            } 
+
+            catch (ClassNotFoundException e) 
+            {
+                e.printStackTrace();
+            }
+
+          //creating connection to betn sql database and driver
+          conn=DriverManager.getConnection(URL, db_user, db_password);
+            
+        } 
+
+        catch (SQLException e) 
+        {
+       
+           e.printStackTrace();
+        }
+    } 
+}   
+  
