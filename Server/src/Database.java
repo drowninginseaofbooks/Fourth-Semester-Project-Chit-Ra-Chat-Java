5ab7abed49ecc697ea12f24ee5d99a55beb057f9
Somu👋 <153536896+somu-sapkota@users.noreply.github.com>
2024-07-30 12:18:22 +0545

    Add files via upload
    Database added

---
+++ Server/src/Database.java
@@ -0,0 +1,10 @@
+//import java.sql.Connection;
+//import java.sql.Statement;
+
+public class Database {
+   
+        String URL = "jdbc:mysql://localhost:3306/chit-ra-chat";
+        String db_user = "root";
+        String db_password = "@s*Omu12";
+     
+}
